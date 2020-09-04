4b8de59fc0911c185554d974b729932a25649b3d
Adele Reed <adreed@microsoft.com>
2020-09-04 14:01:29 -0700

    Add symlinks between upload and download

--- cmd/lhsm-plugin-az-core/archive.go
+++ cmd/lhsm-plugin-az-core/archive.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net/url"
 	"os"
+	"path/filepath"
+	"strings"
 	"syscall"
 
 	"github.com/Azure/azure-storage-blob-go/azblob"
@@ -30,15 +32,48 @@ func Archive(o ArchiveOptions) (int64, error) {
 	defer cancel()
 
 	p := util.NewPipeline(ctx, o.Credential, o.Pacer, azblob.PipelineOptions{})
+
+	// We might actually encounter symlinks here.
+	paths := []string{o.SourcePath}
+
+	for {
+		idx := len(paths) - 1
+
+		// In the original code, the error was ignored, so we're going to maintain the assumption that it's OK to ignore the error here
+		// Though it's not really the most agreeable thing.
+		fi, _ := os.Lstat(paths[idx])
+
+		// If the file is not a symlink, we can jump to uploading it.
+		if fi.Mode()&os.ModeSymlink == 0 {
+			break
+		}
+
+		// Otherwise, read the link and append it.
+		next, err := os.Readlink(paths[idx])
+
+		if err != nil {
+			return 0, err
+		}
+
+		paths = append(paths, next)
+	}
+
+	// Currently, we know the absolute paths, with 0 being the first link in the chain and n being the file itself.
+	// Let's decipher what the actual root is, so we know where our blobs live.
+	// Each of our blob names are going to be strings.TrimPrefix(path, fsRootPath)
+	fsRootPath := strings.TrimSuffix(filepath.Dir(o.SourcePath), filepath.Dir(o.BlobName)) + "/"
+
+	// First, upload the file... We know exactly where this is and what it's going to be named, thus, we'll fix paths[n] so that links go right.
+
+	//// First, upload the file. TODO: Detect if the file and links already exist
+	//// To do this, we need to know the relative paths...
+
 	cURL, _ := url.Parse(fmt.Sprintf("https://%s.blob.core.windows.net/%s", o.AccountName, o.ContainerName))
 	containerURL := azblob.NewContainerURL(*cURL, p)
-	blobURL := containerURL.NewBlockBlobURL(o.ExportPrefix + o.BlobName)
-
-	resp, err := blobURL.GetAccountInfo(context.Background())
-	hnsEnabledAccount := resp.Response().Header.Get("X-Ms-Is-Hns-Enabled") == "true"
+	blobURL := containerURL.NewBlockBlobURL(o.ExportPrefix + strings.TrimSuffix(strings.TrimPrefix(paths[len(paths)-1], fsRootPath), "/"))
 
 	// open the file to read from
-	file, _ := os.Open(o.SourcePath)
+	file, _ := os.Open(paths[len(paths)-1])
 	fileInfo, _ := file.Stat()
 	defer file.Close()
 
@@ -49,6 +84,11 @@ func Archive(o ArchiveOptions) (int64, error) {
 	group := fmt.Sprintf("%d", fileInfo.Sys().(*syscall.Stat_t).Gid)
 	modTime := fileInfo.ModTime().Format("2006-01-02 15:04:05 -0700")
 
+	resp, err := blobURL.GetAccountInfo(context.Background())
+	if err != nil {
+		return 0, err
+	}
+	hnsEnabledAccount := resp.Response().Header.Get("X-Ms-Is-Hns-Enabled") == "true"
 	if !hnsEnabledAccount {
 		meta["Permissions"] = permissions
 		meta["ModTime"] = modTime
@@ -67,11 +107,39 @@ func Archive(o ArchiveOptions) (int64, error) {
 		})
 
 	if err != nil {
-		return total, err
+		return 0, err
 	}
 
 	if hnsEnabledAccount {
 		_, err = blobURL.SetAccessControl(ctx, nil, nil, &owner, &group, &permissions, nil, nil, nil, nil, nil, nil)
+	}
+
+	if err != nil {
+		return total, err
+		//TODO: should we delete blob?
+	}
+
+	//// Prior to creating the links, we need to change what the first link is to match the new destination.
+	//// @Narasimha, this should help you with supporting the custom names.
+	//paths[len(paths)-1] = path.Join(fsRootPath, o.BlobName)
+
+	// Then, create the links. We do it backwards, much like we do for a download.
+	for idx := len(paths) - 2; idx >= 0; idx-- {
+		linkName := strings.TrimPrefix(paths[idx], fsRootPath)
+		linkDest := strings.TrimPrefix(paths[idx+1], fsRootPath)
+
+		_, err = azblob.UploadBufferToBlockBlob(
+			ctx,
+			[]byte(linkDest),
+			containerURL.NewBlockBlobURL(o.ExportPrefix+linkName),
+			azblob.UploadToBlockBlobOptions{
+				Metadata: azblob.Metadata{"ftype": "LNK"},
+			},
+		)
+
+		if err != nil {
+			return 0, err
+		}
 	}
 
 	return total, err
