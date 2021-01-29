4da10568fdd8d50942606cf5e233cbb92fcb653f
Narasimha Kulkarni <63087328+nakulkar-msft@users.noreply.github.com>
2021-01-29 16:58:17 +0530

    Parallelize directory upload

--- cmd/lhsm-plugin-az-core/archive.go
+++ cmd/lhsm-plugin-az-core/archive.go
@@ -1,12 +1,14 @@
 package lhsm_plugin_az_core
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"net/url"
 	"os"
 	"path"
 	"strings"
+	"sync"
 	"syscall"
 
 	"github.com/Azure/azure-pipeline-go/pipeline"
@@ -29,101 +31,120 @@ type ArchiveOptions struct {
 	HNSEnabled    bool
 }
 
-var blobEndPoint string = "https://%s.blob.core.windows.net/"
-var dfsEndPoint string = "https://%s.dfs.core.windows.net/"
+const blobEndPoint string = "https://%s.blob.core.windows.net/"
+const dfsEndPoint string = "https://%s.dfs.core.windows.net/"
+const parallelDirCount = 64 // Number parallel dir metadata uploads
 
-//Archive copies local file to HNS
-func Archive(o ArchiveOptions) (size int64, _ error) {
-	util.Log(pipeline.LogInfo, fmt.Sprintf("Archiving %s", o.BlobName))
-	archiveCtx := context.Background()
-	ctx, cancel := context.WithCancel(archiveCtx)
-	defer cancel()
-
+func upload(ctx context.Context, o ArchiveOptions, blobPath string) (_ int64, err error) {
 	p := util.NewPipeline(ctx, o.Credential, o.Pacer, azblob.PipelineOptions{})
-
 	cURL, _ := url.Parse(fmt.Sprintf(blobEndPoint+"%s%s", o.AccountName, o.ContainerName, o.ResourceSAS))
 	containerURL := azblob.NewContainerURL(*cURL, p)
+	blobURL := containerURL.NewBlockBlobURL(blobPath)
+	meta := azblob.Metadata{}
 
-	parents := strings.Split(o.BlobName, string(os.PathSeparator))
-	blobPath := o.MountRoot // Initialize the parent starting from root.
-	for _, currDir := range parents {
-		blobPath = path.Join(blobPath, currDir) //keep appending path to the url
-		blobURL := containerURL.NewBlockBlobURL(blobPath)
-		meta := azblob.Metadata{}
+	//Get owner, group and perms
+	fileInfo, err := os.Stat(blobPath)
+	if err != nil {
+		util.Log(pipeline.LogError, fmt.Sprintf("Archiving %s. Failed to get fileInfo: %s", blobPath, err.Error()))
+		return 0, err
+	}
 
-		//Get owner, group and perms
-		fileInfo, err := os.Stat(blobPath)
-		if err != nil {
-			util.Log(pipeline.LogError, fmt.Sprintf("Archiving %s. Failed to get fileInfo: %s", o.BlobName, err.Error()))
+	owner := fmt.Sprintf("%d", fileInfo.Sys().(*syscall.Stat_t).Uid)
+	permissions := fmt.Sprintf("%o", fileInfo.Mode())
+	group := fmt.Sprintf("%d", fileInfo.Sys().(*syscall.Stat_t).Gid)
+	modTime := fileInfo.ModTime().Format("2006-01-02 15:04:05 -0700")
+	var getACLResp *azblob.BlobGetAccessControlResponse
+
+	if o.HNSEnabled {
+		dfsEP, _ := url.Parse(fmt.Sprintf(dfsEndPoint+"%s%s", o.AccountName, o.ContainerName, o.ResourceSAS))
+		dfsURL := azblob.NewContainerURL(*dfsEP, p).NewBlockBlobURL(blobPath)
+		getACLResp, err = dfsURL.GetAccessControl(ctx, nil, nil, nil, nil, nil, nil, nil, nil)
+		if stgErr, ok := err.(azblob.StorageError); err != nil || ok && stgErr.ServiceCode() != azblob.ServiceCodeBlobNotFound {
+			util.Log(pipeline.LogError, fmt.Sprintf("Archiving %s. Failed to get Access Control: %s", dfsURL.URL().Path, err.Error()))
 			return 0, err
-		}
-
-		owner := fmt.Sprintf("%d", fileInfo.Sys().(*syscall.Stat_t).Uid)
-		permissions := fmt.Sprintf("%o", fileInfo.Mode())
-		group := fmt.Sprintf("%d", fileInfo.Sys().(*syscall.Stat_t).Gid)
-		modTime := fileInfo.ModTime().Format("2006-01-02 15:04:05 -0700")
-		var acl string
-
-		if o.HNSEnabled {
-			dfsEP, _ := url.Parse(fmt.Sprintf(dfsEndPoint+"%s%s", o.AccountName, o.ContainerName, o.ResourceSAS))
-			dfsURL := azblob.NewContainerURL(*dfsEP, p).NewBlockBlobURL(blobPath)
-			aclResp, err := dfsURL.GetAccessControl(ctx, nil, nil, nil, nil, nil, nil, nil, nil)
-			if stgErr, ok := err.(azblob.StorageError); err != nil || ok && stgErr.ServiceCode() != azblob.ServiceCodeBlobNotFound {
-				util.Log(pipeline.LogError, fmt.Sprintf("Archiving %s. Failed to get Access Control: %s", o.BlobName, err.Error()))
-				return 0, err
-			}
-			if err != nil {
-				acl = aclResp.XMsACL()
-			}
-		}
-
-		if !o.HNSEnabled {
-			meta["Permissions"] = permissions
-			meta["ModTime"] = modTime
-			meta["Owner"] = owner
-			meta["Group"] = group
-		}
-
-		if fileInfo.IsDir() {
-			meta["hdi_isfolder"] = "true"
-			_, err = azblob.UploadBufferToBlockBlob(
-				ctx, nil, blobURL,
-				azblob.UploadToBlockBlobOptions{
-					Metadata: meta,
-				})
-		} else {
-			file, _ := os.Open(blobPath)
-			defer file.Close()
-			size = fileInfo.Size()
-			_, err = azblob.UploadFileToBlockBlob(
-				ctx, file, blobURL,
-				azblob.UploadToBlockBlobOptions{
-					BlockSize:   o.BlockSize,
-					Parallelism: o.Parallelism,
-					Metadata:    meta,
-				})
-		}
-
-		if err != nil {
-			util.Log(pipeline.LogError, fmt.Sprintf("Archiving %s. Failed to upload blob: %s", blobPath, err.Error()))
-			return 0, err
-		}
-
-		if o.HNSEnabled {
-			dfsEP, _ := url.Parse(fmt.Sprintf(dfsEndPoint+"%s%s", o.AccountName, o.ContainerName, o.ResourceSAS))
-			dfsURL := azblob.NewContainerURL(*dfsEP, p).NewBlockBlobURL(blobPath)
-			_, err := dfsURL.SetAccessControl(ctx, nil, nil, nil, nil, nil, &acl, nil, nil, nil, nil, nil)
-			if err != nil {
-				util.Log(pipeline.LogError, fmt.Sprintf("Archiving %s. Failed to set Access Control: %s", blobPath, err.Error()))
-				return 0, err
-			}
-			_, err = dfsURL.SetAccessControl(ctx, nil, nil, &owner, &group, &permissions, nil, nil, nil, nil, nil, nil)
-			if err != nil {
-				util.Log(pipeline.LogError, fmt.Sprintf("Archiving %s. Failed to set owner: %s", blobPath, err.Error()))
-				return 0, err
-			}
 		}
 	}
 
-	return size, nil
+	meta["Permissions"] = permissions
+	meta["ModTime"] = modTime
+	meta["Owner"] = owner
+	meta["Group"] = group
+
+	if fileInfo.IsDir() {
+		meta["hdi_isfolder"] = "true"
+		_, err = blobURL.Upload(ctx, bytes.NewReader(nil), azblob.BlobHTTPHeaders{}, meta, azblob.BlobAccessConditions{}, azblob.AccessTierNone)
+	} else {
+		file, _ := os.Open(blobPath)
+		defer file.Close()
+		_, err = azblob.UploadFileToBlockBlob(
+			ctx, file, blobURL,
+			azblob.UploadToBlockBlobOptions{
+				BlockSize:   o.BlockSize,
+				Parallelism: o.Parallelism,
+				Metadata:    meta,
+			})
+	}
+
+	if err != nil {
+		util.Log(pipeline.LogError, fmt.Sprintf("Archiving %s. Failed to upload blob: %s", blobPath, err.Error()))
+		return 0, err
+	}
+
+	if o.HNSEnabled && getACLResp != nil {
+		dfsEP, _ := url.Parse(fmt.Sprintf(dfsEndPoint+"%s%s", o.AccountName, o.ContainerName, o.ResourceSAS))
+		dfsURL := azblob.NewContainerURL(*dfsEP, p).NewBlockBlobURL(blobPath)
+		acl := getACLResp.XMsACL()
+		owner := getACLResp.XMsOwner()
+		group := getACLResp.XMsGroup()
+		permissions := getACLResp.XMsPermissions()
+		_, err := dfsURL.SetAccessControl(ctx, nil, nil, nil, nil, nil, &acl, nil, nil, nil, nil, nil)
+		if err != nil {
+			util.Log(pipeline.LogError, fmt.Sprintf("Archiving %s. Failed to set Access Control: %s", blobPath, err.Error()))
+			return 0, err
+		}
+		_, err = dfsURL.SetAccessControl(ctx, nil, nil, &owner, &group, &permissions, nil, nil, nil, nil, nil, nil)
+		if err != nil {
+			util.Log(pipeline.LogError, fmt.Sprintf("Archiving %s. Failed to set owner: %s", blobPath, err.Error()))
+			return 0, err
+		}
+	}
+
+	return fileInfo.Size(), err
+}
+
+//Archive copies local file to HNS
+func Archive(o ArchiveOptions) (size int64, err error) {
+	util.Log(pipeline.LogInfo, fmt.Sprintf("Archiving %s", o.BlobName))
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	wg := sync.WaitGroup{}
+
+	parents := strings.Split(o.BlobName, string(os.PathSeparator))
+	parents = parents[:len(parents)-1] // Exclude the file itself (processed separately)
+	wg.Add(len(parents) + 1)           // Parent directories + 1 for the file.
+
+	// Upload the file
+	go func() {
+		size, err = upload(ctx, o, o.SourcePath)
+		wg.Done()
+	}()
+
+	//parallely upload directories starting from root.
+	blobPath := o.MountRoot
+	guard := make(chan struct{}, parallelDirCount) //Guard maintains bounded paralleism for uploading directories
+	defer close(guard)
+	for _, currDir := range parents {
+		blobPath = path.Join(blobPath, currDir) //keep appending path to the url
+
+		guard <- struct{}{} //block till we've enough room.
+		go func() {
+			upload(ctx, o, blobPath)
+			<-guard //release a space in guard.
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+
+	return size, err
 }
